714380877f60dcfb5083b9fbd9b3c6b4b39725b4
GBangad <ganesh.bangad@thalesgroup.com>
2025-04-17 11:36:31 +0800

    updated api doc for Android

---
+++ tmg-sdk/android/3.0.0_beta/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.thalesgroup.tmgsdk"},{"l":"com.thalesgroup.tmgsdk.mastercard"},{"l":"com.thalesgroup.tmgsdk.model"},{"l":"com.thalesgroup.tmgsdk.model.mastercard"},{"l":"com.thalesgroup.tmgsdk.model.visa"},{"l":"com.thalesgroup.tmgsdk.visa"}]
