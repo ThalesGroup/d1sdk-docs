625cf7a2b1b08b3a32809d0b4e0bc29d424ceed1
dipta <hazratpradipta.ranjali@thalesgroup.com>
2025-05-08 18:45:07 +0800

    remove 3.0.0

--- tmg-sdk/android/3.0.0/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.thalesgroup.tmgsdk"},{"l":"com.thalesgroup.tmgsdk.mastercard"},{"l":"com.thalesgroup.tmgsdk.model"},{"l":"com.thalesgroup.tmgsdk.model.mastercard"},{"l":"com.thalesgroup.tmgsdk.model.visa"},{"l":"com.thalesgroup.tmgsdk.visa"}]
